cee243b0b3876e9e122f0601c187d5a0d8d1d4fb
MISTERSOFT <MISTERSOFT@users.noreply.github.com>
2026-03-26 10:33:15 +0100

    chore: auto generate files on first dev run

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import adonisjs from '@adonisjs/vite/client'
 import inertia from '@adonisjs/inertia/vite'
+import adonisjs from '@adonisjs/vite/client'
+import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
 
 export default defineConfig({
   plugins: [
