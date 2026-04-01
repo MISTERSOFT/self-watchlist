054e548c0347248f64befc4e8e40aa4b7106fe7d
MISTERSOFT <MISTERSOFT@users.noreply.github.com>
2026-04-01 15:34:19 +0200

    chore(cli): add command to generate react component template

---
+++ commands/make_react_component.ts
@@ -0,0 +1,53 @@
+import { args, BaseCommand } from '@adonisjs/core/ace'
+import type { CommandOptions } from '@adonisjs/core/types/ace'
+import { join } from 'node:path'
+
+const STUBS_ROOT = join(import.meta.dirname, '../stubs')
+const STUB_PATH = 'make/react/component.stub'
+
+export default class MakeReactComponent extends BaseCommand {
+  static commandName = 'make:react:component'
+  static description = 'Create a new React component'
+
+  static options: CommandOptions = {
+    startApp: false,
+    allowUnknownFlags: false,
+    staysAlive: false,
+  }
+
+  static help?: string | string[] | undefined = [
+    'This command create a new React component inside the Inertia folder (inertia/components).',
+  ]
+
+  @args.string({ description: 'Component name' })
+  declare name: string
+  async run() {
+    await this._generateFile()
+  }
+
+  async completed() {
+    if (this.error) {
+      /**
+       * Handle the error from any lifecycle method
+       */
+      this.logger.error(this.error.message)
+
+      /**
+       * Return true to notify Ace that you've handled the error
+       * This prevents Ace from logging the error again
+       */
+      return true
+    }
+  }
+
+  /**
+   * Generate component file
+   */
+  private async _generateFile() {
+    this.logger.info(`Creating "${this.name}" component...`)
+    const codemods = await this.createCodemods()
+    await codemods.makeUsingStub(STUBS_ROOT, STUB_PATH, {
+      name: this.name,
+    })
+  }
+}
