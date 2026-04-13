894c5cb855182dfba851414857eac621a71dad12
MISTERSOFT <MISTERSOFT@users.noreply.github.com>
2026-04-13 19:25:31 +0200

    chore(ui): display animes in cards

---
+++ inertia/components/media-card.tsx
@@ -0,0 +1,55 @@
+import { Card, CardContent } from '@/components/ui/card'
+import { useTranslations } from '@/hooks/use-translations'
+import { Data } from '@generated/data'
+
+interface MediaCardProps {
+  media: Data.Media
+}
+
+export function MediaCard({ media }: MediaCardProps) {
+  const { t } = useTranslations()
+
+  return (
+    <Card className="py-0 group cursor-pointer transition-all duration-300 hover:shadow-lg bg-card border-border overflow-hidden gap-0">
+      <div className="relative aspect-[3/4] overflow-hidden">
+        <img
+          src={media.thumbnailUrl || '/public/logo_bg_white.png'}
+          alt={media.title}
+          className="object-cover transition-transform duration-300 group-hover:scale-110"
+        />
+        <div className="absolute inset-0 bg-black/60 opacity-0 group-hover:opacity-100 transition-opacity duration-300 flex items-center justify-center">
+          {/* <Play className="h-12 w-12 text-white" /> */}
+        </div>
+      </div>
+
+      <CardContent className="p-4 space-y-3">
+        <div>
+          <h3 className="font-semibold text-foreground line-clamp-2 truncate" title={media.title}>
+            {media.title}
+          </h3>
+          {/* <p className="text-sm text-muted-foreground line-clamp-1">{anime.genres}</p> */}
+        </div>
+
+        {/* <div className="flex items-center justify-between text-xs text-muted-foreground">
+          <div className="flex items-center gap-1">
+            <Calendar className="h-3 w-3" />
+            {media.seasonYear}
+          </div>
+          <div className="flex items-center gap-1">
+            <Play className="h-3 w-3" />
+            {media.episodesCount} épisodes
+          </div>
+        </div>
+
+        <div className="flex items-center justify-between">
+          <div className="flex items-center gap-1">
+            <Star className="h-4 w-4 fill-yellow-400 text-yellow-400" />
+            <span className="text-sm font-medium text-foreground">
+              {media.score || '-'}
+            </span>
+          </div>
+        </div> */}
+      </CardContent>
+    </Card>
+  )
+}
--- inertia/pages/home.tsx
+++ inertia/pages/home.tsx
@@ -1,31 +1,28 @@
-import { InertiaProps } from '@/types';
+import { MediaCard } from '@/components/media-card'
+import { InertiaProps } from '@/types'
 // import { Link } from "@adonisjs/inertia/react";
-import { Data } from "@generated/data";
+import { Data } from '@generated/data'
 
 type PageProps = InertiaProps<{ medias: Data.Media[] }>
 
-export default function Home({}: PageProps) {
+export default function Home({ medias }: PageProps) {
   return (
     <div className="container mx-auto px-4 py-8">
-      {/* <DynamicBreadcrumb /> */}
-
       {/* <AnimeFilters {...data.filters} /> */}
 
       <div className="space-y-8">
         <div className="flex items-center justify-between">
-          <h1 className="text-2xl font-semibold text-foreground">Liste des Animes</h1>
-          <div className="text-sm text-muted-foreground">
-            {/* {data.animes.meta.total} */}0 résultats trouvés
-          </div>
+          <h1 className="text-2xl font-semibold text-foreground">À voir</h1>
+          <div className="text-sm text-muted-foreground">{medias.length} résultats trouvés</div>
         </div>
 
-        {/* <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-6">
-          {data.animes.data.map((anime) => (
-            <Link href={`/animes/${anime.slug}`} key={anime.id}>
-              <AnimeCard anime={anime} />
-            </Link>
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-6">
+          {medias.map((media) => (
+            <MediaCard media={media} key={media.id} />
+            // <Link href={`/animes/${media.id}`} key={media.id}>
+            // </Link>
           ))}
-        </div> */}
+        </div>
 
         {/* <Paginator meta={data.animes.meta} /> */}
       </div>
