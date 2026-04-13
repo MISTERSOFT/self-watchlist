f924f5679d3178e9f05d6edfe4f192784a7a324c
MISTERSOFT <MISTERSOFT@users.noreply.github.com>
2026-04-13 12:03:21 +0200

    chore(config): change eslint behavior for inertia folder

--- eslint.config.js
+++ eslint.config.js
@@ -1,2 +1,18 @@
 import { configApp } from '@adonisjs/eslint-config'
-export default configApp()
+import eslintPluginUnicorn from 'eslint-plugin-unicorn'
+export default configApp([
+  {
+    plugins: {
+      eslintPluginUnicorn,
+    },
+    files: ['inertia/**/*.ts', 'inertia/**/*.js'],
+    rules: {
+      'unicorn/filename-case': [
+        'error',
+        {
+          case: 'kebabCase',
+        },
+      ],
+    },
+  },
+])
