13a18466e365bcc3c0fafb11dbcf7a16ba0eef61
MISTERSOFT <MISTERSOFT@users.noreply.github.com>
2026-04-17 15:04:05 +0200

    chore: add navbar dropdown menu for logged user

---
+++ inertia/components/logged-user-dropdown-menu.tsx
@@ -0,0 +1,101 @@
+import { useImportMyAnimeListDialog } from '@/components/import-my-anime-list-dialog-provider'
+import { useTheme } from '@/components/theme-provider'
+import { Button } from '@/components/ui/button'
+import {
+  DropdownMenu,
+  DropdownMenuCheckboxItem,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuPortal,
+  DropdownMenuSeparator,
+  DropdownMenuSub,
+  DropdownMenuSubContent,
+  DropdownMenuSubTrigger,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu'
+import { InertiaProps } from '@/types'
+import { Link } from '@adonisjs/inertia/react'
+import { ChevronDown, LogOut } from 'lucide-react'
+import { useCallback } from 'react'
+
+interface LoggedUserDropdownMenuProps {
+  user: NonNullable<InertiaProps['user']>
+}
+
+export const LoggedUserDropdownMenu = ({ user }: LoggedUserDropdownMenuProps) => {
+  const { setOpen } = useImportMyAnimeListDialog()
+  const { theme, setTheme } = useTheme()
+
+  const openImportDialog = useCallback(() => setOpen(true), [])
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant="ghost" size="lg">
+          <div className="grid flex-1 text-left text-sm leading-tight">
+            <span className="truncate font-medium">{user.initials}</span>
+            <span className="truncate text-xs">{user.email}</span>
+          </div>
+          <ChevronDown className="ml-auto size-4" />
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent
+        className="w-(--radix-dropdown-menu-trigger-width) min-w-56 rounded-lg"
+        side="bottom"
+        align="end"
+        sideOffset={4}
+      >
+        <DropdownMenuLabel className="p-0 font-normal">
+          <div className="flex items-center gap-2 px-1 py-1.5 text-left text-sm">
+            <div className="grid flex-1 text-left text-sm leading-tight">
+              <span className="truncate font-medium">{user.initials}</span>
+              <span className="truncate text-xs">{user.email}</span>
+            </div>
+          </div>
+        </DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        <DropdownMenuGroup>
+          <DropdownMenuItem onClick={openImportDialog}>Import from MyAnimeList</DropdownMenuItem>
+        </DropdownMenuGroup>
+        <DropdownMenuGroup>
+          <DropdownMenuSub>
+            <DropdownMenuSubTrigger>Theme</DropdownMenuSubTrigger>
+            <DropdownMenuPortal>
+              <DropdownMenuSubContent>
+                <DropdownMenuGroup>
+                  <DropdownMenuCheckboxItem
+                    checked={theme === 'light'}
+                    onCheckedChange={() => setTheme('light')}
+                  >
+                    Light
+                  </DropdownMenuCheckboxItem>
+                  <DropdownMenuCheckboxItem
+                    checked={theme === 'dark'}
+                    onCheckedChange={() => setTheme('dark')}
+                  >
+                    Dark
+                  </DropdownMenuCheckboxItem>
+                  <DropdownMenuCheckboxItem
+                    checked={theme === 'system'}
+                    onCheckedChange={() => setTheme('system')}
+                  >
+                    System
+                  </DropdownMenuCheckboxItem>
+                </DropdownMenuGroup>
+              </DropdownMenuSubContent>
+            </DropdownMenuPortal>
+          </DropdownMenuSub>
+        </DropdownMenuGroup>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem>
+          <Link route="session.destroy" className="inline-flex">
+            <LogOut className="mr-2" />
+            Log out
+          </Link>
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  )
+}
--- inertia/components/navbar.tsx
+++ inertia/components/navbar.tsx
@@ -1,10 +1,8 @@
 import { ImportMyAnimeListDialog } from '@/components/import-my-anime-list-dialog'
+import { LoggedUserDropdownMenu } from '@/components/logged-user-dropdown-menu'
 import { SearchMediaInput } from '@/components/search-media-input'
-import { ThemeModeToggle } from '@/components/theme-mode-toggle'
-import { Button, buttonVariants } from '@/components/ui/button'
-import { cn } from '@/lib/utils'
 import { InertiaProps } from '@/types'
-import { Form, Link } from '@adonisjs/inertia/react'
+import { Link } from '@adonisjs/inertia/react'
 import { usePage } from '@inertiajs/react'
 
 interface NavbarProps {}
@@ -13,39 +11,14 @@ const Navbar = ({}: NavbarProps) => {
   const { props } = usePage<InertiaProps>()
 
   return (
-    <header className="bg-background sticky top-0 z-50">
-      <div className="mx-auto flex max-w-7xl items-center justify-between gap-8 px-4 py-7 sm:px-6">
+    <header className="bg-background/90 backdrop-blur-lg sticky top-0 z-50">
+      <ImportMyAnimeListDialog />
+
+      <nav className="mx-auto flex max-w-7xl items-center justify-between gap-8 px-4 py-7 sm:px-6">
         <div className="text-muted-foreground flex flex-1 items-center gap-8 font-medium md:justify-center lg:gap-16">
           <Link route="home">Self Watchlist</Link>
           <SearchMediaInput />
-          {/* className="pl-8 bg-secondary border-border text-foreground placeholder:text-muted-foreground" */}
-          <nav className="flex gap-2">
-            <ImportMyAnimeListDialog />
-            <ThemeModeToggle />
-            {props.user ? (
-              <>
-                <span>{props.user.initials}</span>
-                <Form route="session.destroy">
-                  <Button type="submit">Logout</Button>
-                </Form>
-              </>
-            ) : (
-              <>
-                <Link
-                  route="new_account.create"
-                  className={cn(buttonVariants({ variant: 'default', size: 'default' }))}
-                >
-                  Signup
-                </Link>
-                <Link
-                  route="session.create"
-                  className={cn(buttonVariants({ variant: 'outline', size: 'default' }))}
-                >
-                  Login
-                </Link>
-              </>
-            )}
-          </nav>
+          <LoggedUserDropdownMenu user={props.user!} />
         </div>
 
         <div className="flex items-center gap-6 md:hidden">
@@ -71,7 +44,7 @@ const Navbar = ({}: NavbarProps) => {
             </DropdownMenuContent>
           </DropdownMenu> */}
         </div>
-      </div>
+      </nav>
     </header>
   )
 }
