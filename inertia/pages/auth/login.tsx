c2e99f2e52cd86d06b13452b2fb30b0e5793e472
MISTERSOFT <MISTERSOFT@users.noreply.github.com>
2026-04-25 17:42:48 +0200

    refacto(login): remove useless commented login form

--- inertia/pages/auth/login.tsx
+++ inertia/pages/auth/login.tsx
@@ -79,41 +79,6 @@ export default function Login() {
             </>
           )}
         </Form>
-
-        {/* <Form route="session.store">
-          {({ errors }) => (
-            <>
-              <div>
-                <label htmlFor="email">Email</label>
-                <input
-                  type="email"
-                  name="email"
-                  id="email"
-                  autoComplete="username"
-                  data-invalid={errors.email ? 'true' : undefined}
-                />
-                {errors.email && <div>{errors.email}</div>}
-              </div>
-
-              <div>
-                <label htmlFor="password">Password</label>
-                <input
-                  type="password"
-                  name="password"
-                  id="password"
-                  autoComplete="current-password"
-                />
-                {errors.password ? <span>{errors.password}</span> : ''}
-              </div>
-
-              <div>
-                <button type="submit" className="button">
-                  Login
-                </button>
-              </div>
-            </>
-          )}
-        </Form> */}
       </div>
     </div>
   )
